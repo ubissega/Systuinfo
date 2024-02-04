eda5ce7aab3f5090f6c47f66d535a2627eeee9c6
ubissega <146642724+ubissega@users.noreply.github.com>
2024-02-04 14:05:17 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+     apiKey: "AIzaSyDohCeHRGxRiO4sfN9pOFHVUqe01cuzUEg",
+        authDomain: "systuinfo.firebaseapp.com",
+        projectId: "systuinfo",
+        storageBucket: "systuinfo.appspot.com",
+        messagingSenderId: "604412345972",
+        appId: "1:604412345972:web:96338d9c4f5853d1765b21"
+};
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
