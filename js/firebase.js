0085b1be4157d14132e45840ff94c11be6079699
ubissega <146642724+ubissega@users.noreply.github.com>
2024-02-05 22:31:24 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,15 @@
-// firebase.js conexao com a firebase BD google
-const firebaseConfig = {
-     apiKey: "AIzaSyDohCeHRGxRiO4sfN9pOFHVUqe01cuzUEg",
-     authDomain: "systuinfo.firebaseapp.com",
-     databaseURL: "https://systuinfo-default-rtdb.firebaseio.com",
-     projectId: "systuinfo",
-     storageBucket: "systuinfo.appspot.com",
-     messagingSenderId: "604412345972",
-     appId: "1:604412345972:web:96338d9c4f5853d1765b21"
-};
-
-// Inicialize o Firebase
-firebase.initializeApp(firebaseConfig);
+document.addEventListener('DOMContentLoaded', function() {
+     // firebase.js conexao com a firebase BD google
+     const firebaseConfig = {
+          apiKey: "AIzaSyDohCeHRGxRiO4sfN9pOFHVUqe01cuzUEg",
+          authDomain: "systuinfo.firebaseapp.com",
+          databaseURL: "https://systuinfo-default-rtdb.firebaseio.com",
+          projectId: "systuinfo",
+          storageBucket: "systuinfo.appspot.com",
+          messagingSenderId: "604412345972",
+          appId: "1:604412345972:web:96338d9c4f5853d1765b21"
+     };
+     
+     // Inicialize o Firebase
+     firebase.initializeApp(firebaseConfig);
+});
